9a4fc35df58e9147f5a8989431b0eb058b70b5e8
LucasWalica <lucasw58@educastur.es>
2025-11-04 12:57:56 +0100

    first home page design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,12 +13,12 @@ export const routes: Routes = [
     {path: "business/detail-by-owner", loadComponent: () => import("./components/business/detail-by-owner/detail-by-owner").then(m=>m.DetailByOwner)},
     {path: "business/detail-by-user", loadComponent: () => import("./components/business/detail-by-user/detail-by-user").then(m=>m.DetailByUser)},
     // services
-    {path: "services/create", loadComponent: () => import("./components/services/create/create").then(m=>m.Create)},
-    {path: "services/delete", loadComponent: () => import("./components/services/delete/delete").then(m=>m.Delete)},
-    {path: "services/detail-by-owner", loadComponent: () => import("./components/services/detail-by-owner/detail-by-owner").then(m=>m.DetailByOwner)},
-    {path: "services/detail-by-user", loadComponent: () => import("./components/services/detail-by-user/detail-by-user").then(m=>m.DetailByUser)},
-    {path: "services/list-by-owner", loadComponent: () => import("./components/services/list-by-owner/list-by-owner").then(m=>m.ListByOwner)},
-    {path: "services/list-by-user", loadComponent: () => import("./components/services/list-by-user/list-by-user").then(m=>m.ListByUser)},
+    {path: "services/create", loadComponent: () => import("./components/businessservices/create/create").then(m=>m.Create)},
+    {path: "services/delete", loadComponent: () => import("./components/businessservices/delete/delete").then(m=>m.Delete)},
+    {path: "services/detail-by-owner", loadComponent: () => import("./components/businessservices/detail-by-owner/detail-by-owner").then(m=>m.DetailByOwner)},
+    {path: "services/detail-by-user", loadComponent: () => import("./components/businessservices/detail-by-user/detail-by-user").then(m=>m.DetailByUser)},
+    {path: "services/list-by-owner", loadComponent: () => import("./components/businessservices/list-by-owner/list-by-owner").then(m=>m.ListByOwner)},
+    {path: "services/list-by-user", loadComponent: () => import("./components/businessservices/list-by-user/list-by-user").then(m=>m.ListByUser)},
     // reservation 
     {path: "reservation/create", loadComponent: () => import("./components/reservation/create/create").then(m=>m.Create)},
     {path: "reservation/update/:token", loadComponent: () => import("./components/reservation/update/update").then(m=>m.Update)},
@@ -31,4 +31,5 @@ export const routes: Routes = [
     {path: "feedback/create", loadComponent: () => import("./components/feedback/create/create").then(m=>m.Create)},
     {path: "feedback/delete/:id", loadComponent: () => import("./components/feedback/delete/delete").then(m=>m.Delete)},
     {path: "feedback/list", loadComponent: () => import("./components/feedback/list/list").then(m=>m.List)},
+    {path: "**", redirectTo: "" },
 ];
